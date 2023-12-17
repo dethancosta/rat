5b6091c08cd4c5b970739845562554747bf01fa9
D E Costa <d.ethan.costa@gmail.com>
2023-12-17 14:46:53 -0500

    Complete Step 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 pub mod rat;
 
-use std::env;
+use std::{env, io};
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    rat::get_contents(&args[1]).unwrap()
+    if args.len() != 2 {
+        println!("No input provided");
+        std::process::exit(1);
+    } else if args[1] == "-" {
+        rat::get_contents(io::stdin()).unwrap()
+    } else {
+        let mut f: File = File::open(&args[1]).unwrap();
+        rat::get_contents(&mut f).unwrap()
+    }
 }
--- src/rat.rs
+++ src/rat.rs
@@ -1,8 +1,7 @@
-use std::{io::{self, BufReader, BufRead}, fs::File};
+use std::io::{self, BufReader, BufRead, Read};
 
-pub fn get_contents(filename: &String) -> Result<(), io::Error> {
-    let f = File::open(filename)?;
-    let mut reader = BufReader::new(f);
+pub fn get_contents<T: Read>(source: T) -> Result<(), io::Error> {
+    let mut reader = BufReader::new(source);
     let mut buf = String::new();
     loop {
         let result = reader.read_line(&mut buf)?;
